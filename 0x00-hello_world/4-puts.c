5345fbb358b3867da29c42789f75b30bfbd8106c
Olaoluwa <anigboronapoleonolaoluwa@gmail.com>
2022-09-08 08:51:58 -0700

    Modified task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts("Programming is like a multilingual puzzle"); return (0);
+#include <stdio.h>
+
+/* a function to print using puts */
+char main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
