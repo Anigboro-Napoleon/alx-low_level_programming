0716b6c99b9b3dfb9197fc4a89215c2ebef095b7
Olaoluwa <anigboronapoleonolaoluwa@gmail.com>
2022-09-08 15:50:53 -0700

    Modified task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-/* a function to print using puts */
-char main(void)
+/**
+ * Description: main - Prints out a string to stdout
+ * Return: 0 if success
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
