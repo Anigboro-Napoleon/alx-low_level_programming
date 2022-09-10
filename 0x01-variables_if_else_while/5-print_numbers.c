b699dd9d9efc6bdfed903ede9ebecb53e09bb371
Olaoluwa <anigboronapoleonolaoluwa@gmail.com>
2022-09-10 01:10:48 -0700

    Completed task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers startung from 0
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
