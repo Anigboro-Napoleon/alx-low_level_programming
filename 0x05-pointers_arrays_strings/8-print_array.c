59b0f3b72ff839901e27d51895adcef18a094cd3
Olaoluwa <anigboronapoleonolaoluwa@gmail.com>
2022-09-20 09:50:11 -0700

    8. Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - print `n` elements of an array of integers
@@ -6,6 +7,7 @@
  * @n: int type integer
  * Description: Numbers must be separated by comma and space.
  * Numbers should be displayed in the same order they are stored in array.
+ * You can only use _putchar to print.
  */
 
 void print_array(int *a, int n)
@@ -13,7 +15,7 @@ void print_array(int *a, int n)
 	int i;
 
 	i = 0;
-	for (n--; n >= 0; n--; i++)
+	for (n--; n >= 0; n--, i++)
 	{
 		printf("%d", a[i]);
 		if (n > 0)
